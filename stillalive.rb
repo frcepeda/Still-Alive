1ecd8bb1abf9e1cb4a48a11493708ea7b9916b88
Freddy Román <freddy@frcepeda.com>
2012-06-04 00:24:21 -0500

    Added --notiming and --noclear options.

--- stillalive.rb
+++ stillalive.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+require 'optparse'
+
 trap('SIGINT') { puts; exit 1 }
 trap('SIGWINCH') { getScreenSize }
 
@@ -23,7 +25,8 @@ def getScreenSize
     end
 end
 
-BEATSIZE = 0.5
+$clear = true
+$beatsize = 0.5
 x = 2
 
 $logo = "*               .,-:;#;:=,*           . :H@@@MM@M#H/.,+%;,*        ,/X+ +M@@M@MM%=,-%HMMM@X/,*      -+@MM; $M@@MH+-,;XMMMM@MMMM@+-*     ;@M@@M- XM@X;. -+XXXXXHHH@M@M\#@/.*   ,%MM@@MH ,@%=            .---=-=:=,.*   =@\#@@@MX .,              -%HX$$%%%+;*  =-./@M@M$                  .;@MMMM@MM:*  X@/ -$MM/                    .+MM@@@M$* ,@M@H: :@:                    . =X\#@@@@-* ,@@@MMX, .                    /H- ;@M@M=* .H@@@@M@+,                    %MM+..%\#$.*  /MMMM@MMH/.                  XM@MH; =;*   /%+%$XHH@$=              , .H@@@@MX,*    .=--------.           -%H.,@@@@@MX,*    .%MM@@@HHHXX$$$%+= .:$MMX =M@@MM%.*      =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=*        =%@M@M\#@$-.=$@MM@@@M; %M%=*          ,:+$+-,/H#MMMMMMM@= =,*                =++%%%%+/:-."
@@ -32,12 +35,12 @@ $cake = "*            ,:/+/-*            /M/              .,-=;#;-*       .:/= ;
 
 class Object
     def beats
-        return self * BEATSIZE
+        return self * $beatsize
     end
 end
 
 def writeLine(line, beats)
-    puts ""
+    puts
     begin
         stars = line.scan(/[*]/).count
     rescue
@@ -60,7 +63,7 @@ def writeChar(line, beats, i)
 end
 
 def drawApertureLogo
-    puts ""
+    puts
     for char in $logo.split(//)
         if char == '*'
             puts
@@ -74,7 +77,7 @@ def drawApertureLogo
 end
 
 def drawCake
-    puts ""
+    puts
     for char in $cake.split(//)
         if char == '*'
             puts
@@ -87,16 +90,32 @@ def drawCake
     return 10
 end
 
+OptionParser.new do |opts|
+	opts.banner = "Usage: ./letscrate.rb [options]"
+
+	opts.on("-c", "--noclear", "Don't clear the screen before starting") do
+		$clear = false
+	end
+
+	opts.on("--notiming", "Print everything at once.") do
+		$beatsize = 0
+	end
+end.parse!
+
 getScreenSize
 
-printf "3.."
-sleep 1.beats
-printf "2.."
-sleep 1.beats
-printf "1.."
-sleep 1.beats
+if $beatsize != 0
+	printf "3.."
+	sleep 1.beats
+	printf "2.."
+	sleep 1.beats
+	printf "1.."
+	sleep 1.beats
+end
 
-($lines - 1).times { puts }
+if $clear and not $beatsize == 0
+	system("clear")
+end
 
 writeLine "This was a triumph", 2.5
 sleep 5.beats
@@ -106,7 +125,7 @@ writeLine "HUGE SUCCESS.", 2.5
 sleep 3.beats
 writeLine "It's hard to overstate my satisfaction.", 10
 sleep 5.beats
-puts ""
+puts
 writeLine "Aperture Science", 2.5
 sleep 5.beats
 writeLine "We do what we must** because** we can.", 5
@@ -115,7 +134,7 @@ writeLine "For the good of all of us", 6
 sleep 0.5.beats
 writeLine "Except the ones who are dead", 3.5
 sleep 1.beats
-puts ""
+puts
 writeLine "But there's no sense crying over every mistake.", 7.5
 sleep 0.5.beats
 writeLine "You just keep on trying till you run out of cake.", 7.5
@@ -141,7 +160,7 @@ writeLine "As they burned** it hurt because", 5
 sleep 0.5.beats
 writeLine "I was so happy for you!", 3.5
 sleep 1.5.beats
-puts ""
+puts
 writeLine "Now these points of data make a beautiful line.", 7.5
 sleep 0.5.beats
 writeLine "And we're out of beta.", 4
@@ -154,7 +173,7 @@ writeLine "Think of all the things we learned", 3.5
 sleep 0.5.beats
 writeLine "For the people who are still alive.", 6
 sleep (13 - drawCake).beats
-puts ""
+puts
 writeLine "Go ahead and leave me.", 3
 sleep 4.5.beats
 writeLine "I think I'd prefer** to stay** inside.", 5
@@ -169,7 +188,7 @@ writeLine "Anyway, this cake is great.", 6
 sleep 0.5.beats
 writeLine "It's so delicious and moist.", 3.5
 sleep 1.beats
-puts ""
+puts
 writeLine "Look at me still talking when there's Science to do.", 7.5
 sleep 0.5.beats
 writeLine "When I look out there, it makes me GLaD I'm not you.", 7.5
@@ -180,7 +199,7 @@ writeLine "there is research to be done.", 3.5
 sleep 0.5.beats
 writeLine "On the people who are still alive.", 5.5
 sleep 2.beats
-puts ""
+puts
 writeLine "And believe me I am still alive.", 6
 sleep 1.5.beats
 writeLine "I'm doing Science and I'm still alive.", 6.5
@@ -191,10 +210,9 @@ writeLine "While you're dying I'll be still alive.", 6
 sleep 1.5.beats
 writeLine "And when you're dead I will be still alive.", 6
 sleep 2.beats
-puts ""
+puts
 writeLine "Still alive", 2.5
 sleep 1.5.beats
 writeLine "STILL ALIVE", 2.5
 sleep 3.beats
-puts ""
-puts ""
+puts
